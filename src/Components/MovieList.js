b5b00f0dcc5e5b6ea9664645373d7c1921de89b0
stephaniepops <59659213+stephaniepops@users.noreply.github.com>
2021-01-16 21:30:09 -0600

    Adjusted Movie List Styling

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -3,17 +3,24 @@ import MovieListItem from "./MovieListItem";
 import { useSelector } from "react-redux";
 import { searchResultSelector } from "../reducers/searchResults";
 import { ADD_NOMINATION } from "./MovieListItem";
+import styled from "@emotion/styled/macro";
+
+const MovieContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 const MovieList = () => {
   const movieListData = useSelector(searchResultSelector);
 
   return (
-    <div>
+    <MovieContainer>
       {movieListData &&
         movieListData.map((movie, index) => (
           <MovieListItem key={index} movie={movie} variant={ADD_NOMINATION} />
         ))}
-    </div>
+    </MovieContainer>
   );
 };
 
