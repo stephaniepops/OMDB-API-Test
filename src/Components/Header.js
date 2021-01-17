367b60b1f6764e34b105889c000b8f254b952936
stephaniepops <59659213+stephaniepops@users.noreply.github.com>
2021-01-16 21:06:32 -0600

    Added Logo

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,9 +5,19 @@ import MovieList from "./MovieList";
 
 const HeaderStyle = styled.div``;
 
+const Logo = styled.img`
+  width: 100%;
+  max-width: 350px;
+  height: auto;
+`;
+
 const Header = () => {
   return (
     <HeaderStyle>
+      <Logo
+        src={process.env.PUBLIC_URL + "ShoppiesLogo.png"}
+        alt='shoppies logo'
+      />
       <SearchForm />
       <MovieList />
     </HeaderStyle>
